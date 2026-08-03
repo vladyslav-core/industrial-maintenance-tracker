a909f6d1fbabd9455aa4e81b8ebc77ee1e68511f
Vladyslav Petryshyn <257185062+vladyslav-core@users.noreply.github.com>
2026-08-03 20:47:30 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/vladyslav/industrialmaintenancetracker/IndustrialMaintenanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vladyslav.industrialmaintenancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialMaintenanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialMaintenanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladyslav/industrialmaintenancetracker/IndustrialMaintenanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladyslav.industrialmaintenancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialMaintenanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
